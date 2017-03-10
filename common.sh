31dfeab653dd66e4535f2deaba4f07b33a56ee38
karabaja4 <karabaja4@gmail.com>
2017-03-10 05:53:41 +0000

    updates

--- common.sh
+++ common.sh
@@ -8,13 +8,13 @@ log()
 declare config_file="config.json"
 declare tmp_dir="/tmp/do_certbot"
 
-declare api_token="$(cat $(pwd)/$config | jq \".api_token\")"
-declare master_domain="$(cat $(pwd)/$config | jq \".master_domain\")"
+declare api_token="$(cat $(pwd)/$config_file | jq \".api_token\")"
+declare master_domain="$(cat $(pwd)/$config_file | jq \".master_domain\")"
 
 declare content_type="Content-Type: application/json"
 declare auth_header="Authorization: Bearer $api_token"
 declare api_url="https://api.digitalocean.com/v2/domains/$master_domain/records"
 
 # ensure tmp dir
-mkdir -p "$tmp"
+mkdir -p "$tmp_dir"
 
