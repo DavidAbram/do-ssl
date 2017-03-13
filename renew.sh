b3b3b611ddb01dfd7b0a46e22f13afedd15f5d50
Igor Saric <karabaja4@gmail.com>
2017-03-13 11:30:46 +0000

    updates

--- renew.sh
+++ renew.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 declare base_dir="$(dirname $(readlink -f $0))"
 declare renew_hook="$base_dir/scripts/renew-hook.sh"
+source "$base_dir/scripts/common.sh"
+
+log "Renewing local SSL certificates"
 
 certbot renew --force-renewal --preferred-challenges dns \
 --test-cert \
