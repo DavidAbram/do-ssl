81a89450fc59c20e0893460c2a749bb3124a631e
Igor Saric <karabaja4@gmail.com>
2017-03-16 22:35:19 +0000

    updates

--- issue.sh
+++ issue.sh
@@ -22,7 +22,7 @@ do
 
     if [ "${#domains[@]}" -gt 0 ]
     then
-	
+
         declare params=$(IFS=, ; echo "${domains[*]}")
         declare email="user@$master_domain"
         log "certbot parameter: -d $params --email $email"
@@ -31,8 +31,8 @@ do
         --agree-tos --email $email --noninteractive --manual-public-ip-logging-ok \
         --manual-auth-hook $auth_hook --manual-cleanup-hook $cleanup_hook
 
-        #declare lineage="/etc/letsencrypt/live/${domains[0]}"
-        #upload $lineage $params
+        declare lineage="/etc/letsencrypt/live/${domains[0]}"
+        upload $lineage $params
 
     else
         # all domains read
